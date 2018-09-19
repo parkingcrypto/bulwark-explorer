0c460bac4682b9b446b82b79d44407ab19f7d3dc
hexter2018 <38841538+hexter2018@users.noreply.github.com>
2018-09-19 21:28:28 +0700

    update link

--- client/component/Card/CardLinks.jsx
+++ client/component/Card/CardLinks.jsx
@@ -7,7 +7,7 @@ const CardLinks = () => (
   <Card title="Links">
     <a href="https://parkcoin.club/" target="_blank">Website</a><br />
     <a href="https://bitcointalk.org/" target="_blank">BitcoinTalk</a><br />
-    <a href="https://github.com/" target="_blank">Github</a><br />
+    <a href="https://github.com/ParkingCoin/parking" target="_blank">Github</a><br />
     <a href="http://discord.me/Pw642Tn" target="_blank">Discord</a><br />
     <a href="https://twitter.com/" target="_blank">Twitter</a><br />
     <a href="https://t.me/" target="_blank">Telegram</a><br />
